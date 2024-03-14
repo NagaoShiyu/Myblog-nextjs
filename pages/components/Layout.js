d997fe716b0ea63cd45f1d56a817835867a250f4
Shiyu <siyuu11660520@gmail.com>
2024-03-15 01:35:51 +0900

    ok

--- pages/components/Layout.js
+++ pages/components/Layout.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 import styles from "./Layout.module.css";
+import utilStyles from "/styles/utils.module.css";
 
 const name= "Ameiba Blog"
 export const siteTitle = "Next.js Started"
@@ -8,11 +9,11 @@ export const siteTitle = "Next.js Started"
 function Layout({children}) {
     return (
         <div className = {styles.container}>
-            <head>
+            <Head>
                 <Link rel="icon" href="/favicon.ico"/>
-            </head>
-            <header>
-                <img src="/images/Aimeiba_logo.jpg"/>
+            </Head>
+            <header className={styles.header}>
+                <img src="/images/Aimeiba_logo2.jpg"className={utilStyles.borderCircle}/>
                 <h1>{name}</h1>
             </header>
             <main>{children}</main>
