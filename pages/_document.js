0e48f7e031845db98bc32e18d200cfa999c6cdea
Shiyu <163304604+NagaoShiyu@users.noreply.github.com>
2024-03-16 12:47:30 +0900

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,10 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript,body } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+        <body>
         <Main />
         <NextScript />
       </body>
